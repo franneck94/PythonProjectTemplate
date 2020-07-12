26050497205c8b2b296d56fd94d780899f15a212
franneck94 <jan2402@DESKTOP-2Q3NQUR.localdomain>
2020-07-12 12:08:17 +0200

    Added more unittests

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -12,6 +12,12 @@ class VectorTests(unittest.TestCase):
         self.v2 = Vector2D(-1, 1)
         self.v3 = Vector2D(2.5, -2.5)
 
+    def test_init(self):
+        """ Tests exceptions for the constructor
+        """
+        self.assertRaises(TypeError, Vector2D, 0, "a")
+        self.assertRaises(TypeError, Vector2D, "B", 1)
+    
     def test_comparison(self):
         """ Tests the comparison operators.
         """
@@ -49,19 +55,26 @@ class VectorTests(unittest.TestCase):
     def test_mul(self):
         """ Tests the multiplication operator.
         """
+        # Valid multiplication
         result1 = self.v1 * 5
         expected_result1 = Vector2D(0.0, 0.0)
         self.assertEqual(result1, expected_result1)
         result2 = self.v1 * self.v2
         expected_result2 = 0.0
         self.assertEqual(result2, expected_result2)
+        # Invalid multiplication
+        self.assertRaises(TypeError, self.v1.__mul__, "a")
 
     def test_div(self):
         """ Tests the multiplication operator.
         """
+        # Valid division
         result = self.v3 / 5
         expected_result = Vector2D(0.5, -0.5)
         self.assertEqual(result, expected_result)
+        # Invalid division
+        self.assertRaises(TypeError, self.v1.__truediv__, "a")
+        self.assertRaises(ValueError, self.v1.__truediv__, 0)
 
 if __name__ == "__main__":
     unittest.main()
