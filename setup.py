400393f97d537a48e2e844b14f0a3e4d726763d0
franneck94 <schaffranek.jan@web.de>
2022-05-20 08:43:47 +0200

    up

--- setup.py
+++ setup.py
@@ -1,67 +1,59 @@
-# python setup.py develop
+# Version 1: python setup.py install <=> pip install .
+# Version 2: python setup.py develop <=> pip install -e .
 from Cython.Build import cythonize
 from setuptools import Extension
 from setuptools import setup
 
 
-CLASSIFIERS = '''\
+CLASSIFIERS = """\
 License :: OSI Approved
-Programming Language :: Python :: 3.7 :: 3.8
+Programming Language :: Python :: 3
 Topic :: Software Development
 Operating System :: Microsoft :: Windows
 Operating System :: POSIX
 Operating System :: Unix
 Operating System :: MacOS
-'''
+"""
 
-DISTNAME = 'fastvector'
-AUTHOR = 'Jan Schaffranek'
-AUTHOR_EMAIL = 'jan.schaffranek@email.com'
-DESCRIPTION = 'This is a simple vector python package.'
-LICENSE = 'MIT'
-README = 'This is a simple vector python package.'
+DISTNAME = "fastvector"
+AUTHOR = "Jan Schaffranek"
+AUTHOR_EMAIL = "jan.schaffranek@email.com"
+DESCRIPTION = "This is a simple vector python package."
+LICENSE = "MIT"
+README = "This is a simple vector python package."
 
-VERSION = '0.1.0'
+VERSION = "0.1.0"
 ISRELEASED = False
 
-PYTHON_MIN_VERSION = '3.7'
-PYTHON_MAX_VERSION = '3.8'
-PYTHON_REQUIRES = f'>={PYTHON_MIN_VERSION}, <={PYTHON_MAX_VERSION}'
+PYTHON_MIN_VERSION = "3.7"
+PYTHON_REQUIRES = f">={PYTHON_MIN_VERSION}"
 
-INSTALL_REQUIRES = [
-    'numpy>=1.18',
-    'scipy',
-    'Cython'
-]
+INSTALL_REQUIRES = ["numpy", "scipy", "Cython"]
 
-PACKAGES = [
-    'fastvector',
-    'tests',
-    'benchmarks'
-]
+PACKAGES = ["fastvector", "tests"]
 
-CYTHON_EXTENSION = [
+CYTHON_EXTENSIONS = [
     Extension(
-        name='fastvector.cython_computations',
-        sources=['fastvector/cython_computations.pyx']
+        name="fastvector.cython_computations",
+        sources=["fastvector/cython_computations.pyx"]
     )
 ]
 
-EXT_MODULES = cythonize(CYTHON_EXTENSION)
+EXT_MODULES = cythonize(CYTHON_EXTENSIONS, language_level="3")
 
 metadata = dict(
     name=DISTNAME,
     version=VERSION,
     long_description=README,
     packages=PACKAGES,
-    ext_modules=EXT_MODULES,
     python_requires=PYTHON_REQUIRES,
     install_requires=INSTALL_REQUIRES,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     description=DESCRIPTION,
     classifiers=[CLASSIFIERS],
-    license=LICENSE
+    license=LICENSE,
+    ext_modules=EXT_MODULES,
 )
 
 
@@ -69,5 +61,5 @@ def setup_package() -> None:
     setup(**metadata)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup_package()
