51af1ea89b736a9962caeade4343fb5320c23666
franneck94 <schaffranek.jan@web.de>
2023-05-25 09:28:52 +0200

    some changes

--- examples/main.py
+++ examples/main.py
@@ -2,9 +2,9 @@ import fastvector
 
 
 def main() -> None:
-    V1 = fastvector.Vector2D(-1, 1)
-    V2 = fastvector.Vector2D(2.5, -2.5)
-    print(V1 - V2)
+    vec1 = fastvector.Vector2D(-1, 1)
+    vec2 = fastvector.Vector2D(2.5, -2.5)
+    print(vec1 - vec2)
 
 
 if __name__ == "__main__":
