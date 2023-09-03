de7bed4172d50da24893d73a3d75e5526f7fe07d
Marios Karagiannopoulos <mariosk@gmail.com>
2023-09-03 21:21:53 +0300

    Fixing lint errors

--- examples/main.py
+++ examples/main.py
@@ -1,7 +1,12 @@
+"""
+This module contains examples of Python code.
+"""
+
 import fastvector
 
 
 def main() -> None:
+    """Main function."""
     vec1 = fastvector.Vector2D(-1, 1)
     vec2 = fastvector.Vector2D(2.5, -2.5)
     print(vec1 - vec2)
